2588a77446957797e3dd92db9a0404317ff33197
lambert <lambert@jcube.com.cn>
2024-08-18 02:07:12 +0800

    no message

--- backend/app/controller/Index.php
+++ backend/app/controller/Index.php
@@ -3,11 +3,14 @@
 namespace app\controller;
 
 use app\BaseController;
+use think\facade\Db;
+use think\facade\Config;
 
 class Index extends BaseController
 {
     public function index()
     {
+		$this->checkConnection();
         return '<style>*{ padding: 0; margin: 0; }</style><iframe src="https://www.thinkphp.cn/welcome?version=' . \think\facade\App::version() . '" width="100%" height="100%" frameborder="0" scrolling="auto"></iframe>';
     }
 
@@ -15,4 +18,16 @@ class Index extends BaseController
     {
         return 'hello,' . $name;
     }
+
+	public function checkConnection()
+	{
+		try {
+			// 尝试连接数据库
+			$re = Db::query('SELECT * from test limit 1');print_r($re);die;
+			return json(['status' => 'success', 'message' => 'Database connection is successful.']);
+		} catch (\Exception $e) {
+			// 捕获异常并返回错误信息
+			return json(['status' => 'error', 'message' => 'Database connection failed: ' . $e->getMessage()]);
+		}
+	}
 }
---
+++ db/lambert-admin.sql
@@ -0,0 +1,16 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : docker-database
+ Source Server Type    : MariaDB
+ Source Server Version : 100616 (10.6.16-MariaDB-1:10.6.16+maria~ubu2004)
+ Source Host           : localhost:6033
+ Source Schema         : lambert-admin
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100616 (10.6.16-MariaDB-1:10.6.16+maria~ubu2004)
+ File Encoding         : 65001
+
+ Date: 18/08/2024 02:07:01
+*/
+
