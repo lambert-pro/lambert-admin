ae67d1ff4b3fc68e874a0d9369474e8313cb9357
lambert <lambert@jcube.com.cn>
2024-08-20 10:49:45 +0800

    重新安装默认的vue3 + 登录页面

---
+++ frontend/src/components/LoginComponents.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="login-container">
+        <div class="login-box">
+            <h1 class="title">Login</h1>
+            <input v-model="username" type="text" placeholder="Username" class="input" />
+            <input v-model="password" type="password" placeholder="Password" class="input" />
+            <button @click="handleLogin" class="login-button">Log in</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import '@/assets/css/login.css';
+
+const username = ref('');
+const password = ref('');
+
+const handleLogin = () => {
+    console.log('Login attempted with', username.value, password.value);
+};
+</script>
